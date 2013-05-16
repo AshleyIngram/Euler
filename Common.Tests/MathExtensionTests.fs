d7656554ed48a8897efbbcda0696a82a40874dd0
Ashley Ingram <ashley2504@hotmail.com>
2013-05-16 20:21:58 +0100

    Added some unit tests

---
+++ Common.Tests/MathExtensionTests.fs
@@ -0,0 +1,31 @@
+﻿namespace AshleyIngram.Euler.Test.Common.MathExtensionTests
+
+open NUnit.Framework
+open FsUnit
+
+open AshleyIngram.Euler.Common.MathExtensions
+
+[<TestFixture>]
+type ``Fibonacci Sequence Tests``()=
+    [<Test>]
+    member x.``The fibonacci sequence is generated correctly``()=
+        let nums = Math.FibonacciSequence |> Seq.take 10 |> Seq.toList;
+        nums.Length |> should equal 10
+        nums |> should equal [1; 2; 3; 5; 8; 13; 21; 34; 55; 89]
+
+[<TestFixture>]
+type ``Greatest Common Factor Tests``()=
+    [<Test>]
+    member x.``Greatest Common Factor for 2 numbers is calculated correctly``()=
+        Math.GreatestCommonFactor(24I, 36I) |> should equal 12I
+
+[<TestFixture>]
+type ``Lowest Common Multiple Tests``()=
+    [<Test>]
+    member x.``Lowest Common Multiple for 2 numbers is correct``()=
+        let list: bigint list = [24I; 36I];
+        Math.LowestCommonMultiple(list) |> should equal 72I
+
+    [<Test>]
+    member x.``Lowest Common Multiple for more than 2 numbers is correct``() =
+        Math.LowestCommonMultiple([1I .. 1I .. 10I]) |> should equal 2520I
---
+++ Common.Tests/StringExtensionTests.fs
@@ -0,0 +1,26 @@
+﻿namespace AshleyIngram.Euler.Test.Common.StringExtensionTests
+
+open NUnit.Framework
+open FsUnit
+
+open AshleyIngram.Euler.Common.StringExtensions.String
+
+[<TestFixture>]
+type ConvertFromCharArrayTests ()=
+    [<Test>]
+    member x.``it converts to a char array to a String successfully`` ()=
+            System.String.FromCharArray([|'h'; 'e'; 'l'; 'l'; 'o'|]) |> should equal "hello";
+
+[<TestFixture>]
+type PalindromeTests ()=
+    [<Test>] 
+    member x.``It returns true for a palindrome`` ()=
+            // odd number of characters
+            "aba".IsPalindrome |> should equal true;
+
+            // even number of characters
+            "abba".IsPalindrome |> should equal true;
+
+    [<Test>] 
+    member x.``It returns false for a non-palindrome`` ()=
+            "abc".IsPalindrome |> should equal false;
