f6a374d30a2f40b26e8805620ea9123f2482d6a9
Ashley Ingram <ashley2504@hotmail.com>
2013-06-18 23:11:03 +0100

    Unit Test Changes
    Adding unit tests for Solution 3 and updating ensuring tests still run
    with new versions of fsharp.core

---
+++ EulerTests/Euler3.fs
@@ -0,0 +1,12 @@
+﻿namespace AshleyIngram.Euler.Test.Solutions
+
+open NUnit.Framework
+open FsUnit
+
+open AshleyIngram.Euler.Problem3
+
+[<TestFixture>]
+type Solution3 ()=
+    [<Test>]
+    member x.``The solution calculates the correct answer for the provided sample`` () =
+        solution 13195UL |> should equal 29
--- Problem3/Euler3.fs
+++ Problem3/Euler3.fs
@@ -1,18 +1,22 @@
-﻿open System;
+﻿module AshleyIngram.Euler.Problem3
+
+open System;
 open AshleyIngram.Euler.Common.MathExtensions
 
 /// <summary>
 /// The prime factors of 13195 are 5, 7, 13 and 29.
 /// What is the largest prime factor of the number 600851475143?
 /// </summary>
+let solution input =
+    let allPrimes = Math.PrimeGenerator(input);
+    let primeFactors = allPrimes |> List.filter(fun n -> input % n = 0UL);
+    primeFactors.Item(primeFactors.Length - 1)
+
+
 [<EntryPoint>]
 let main argv = 
     let input = 600851475143UL;
-
-    let allPrimes = Math.PrimeGenerator(input);
-    let primeFactors = allPrimes |> List.filter(fun n -> input % n = 0UL);
-    let ans = primeFactors.Item(primeFactors.Length - 1)
-
+    let ans = solution input
     Console.WriteLine("Highest Prime Factor is " + ans.ToString());
     Console.ReadLine() |> ignore;
     0 // return an integer exit code
