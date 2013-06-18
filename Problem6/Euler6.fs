0ba3942d522f859641b4ac73d8705cc69aed18bf
Ashley Ingram <ashley2504@hotmail.com>
2013-06-18 21:02:48 +0100

    Solution to Problem 6

--- Problem6/Euler6.fs
+++ Problem6/Euler6.fs
@@ -1,7 +1,19 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
+/// <summary>
+/// The sum of the squares of the first ten natural numbers is 385
+/// The square of the sum of the first ten natural numbers is 3025
+/// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+///
+/// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+/// </summary> 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let sumOfSquares = [1 .. 1 .. 100] |> List.map(fun x -> x*x) |> List.sum
+    let squareOfSum = [1 .. 1 .. 100] |> List.sum |> fun x -> x*x
+
+    let ans = Math.Abs(sumOfSquares - squareOfSum)
+
+    printfn "Answer = %A" ans
+    Console.ReadLine() |> ignore;
     0 // return an integer exit code
