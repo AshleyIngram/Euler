297fb9e832e5d152853f7b1d519b30d5708c41a5
Ashley Ingram <ashley2504@hotmail.com>
2013-06-18 21:30:53 +0100

    Remove some redundancy in calculation
    Remove the absolute check when calculating the difference between the 2
    numbers as the square of the sum is always greater than the sum of the
    squares.

--- Problem6/Euler6.fs
+++ Problem6/Euler6.fs
@@ -3,7 +3,7 @@
 /// <summary>
 /// The sum of the squares of the first ten natural numbers is 385
 /// The square of the sum of the first ten natural numbers is 3025
-/// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+/// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 ///
 /// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 /// </summary> 
@@ -12,7 +12,7 @@ let main argv =
     let sumOfSquares = [1 .. 1 .. 100] |> List.map(fun x -> x*x) |> List.sum
     let squareOfSum = [1 .. 1 .. 100] |> List.sum |> fun x -> x*x
 
-    let ans = Math.Abs(sumOfSquares - squareOfSum)
+    let ans = squareOfSum - sumOfSquares
 
     printfn "Answer = %A" ans
     Console.ReadLine() |> ignore;
